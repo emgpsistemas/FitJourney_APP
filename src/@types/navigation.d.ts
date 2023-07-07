42025a64eeb8c17d5f6e571faa2a7fdc616acdc3
Eduardo Muchak <eduardomuchak@gmail.com>
2023-07-07 14:05:35 -0300

    feat: :sparkles: adds initial routes type file

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: unknown;
+      Exercises: unknown;
+      Training: unknown;
+      Profile: unknown;
+    }
+  }
+}
