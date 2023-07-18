4a714b37506d44f8b74d96b5d9696581e6f4ee18
Eduardo Muchak <eduardomuchak@gmail.com>
2023-07-18 17:49:19 -0300

    feat: :sparkles: implements the registered exercises initial screen structure

--- src/screens/Common/RegisteredExercises/index.tsx
+++ src/screens/Common/RegisteredExercises/index.tsx
@@ -1,21 +1,37 @@
-import { useNavigation } from "@react-navigation/native";
-import { Text, View } from "react-native";
-import { FitButton } from "../../../components/ui/FitButton";
+import { useNavigation } from '@react-navigation/native';
+import { Plus } from 'phosphor-react-native';
+import { FlatList, SafeAreaView, Text, View } from 'react-native';
+import { FitButton } from '../../../components/ui/FitButton';
 
 export default function RegisteredExercises() {
-  const navigation = useNavigation();
-
-  function handleRegisterExercise() {
-    navigation.navigate("RegisterExercise");
-  }
+  const { navigate } = useNavigation();
 
   return (
-    <View className="flex h-screen w-screen items-center justify-center ">
-      <Text className="font-openBold text-xl text-zinc-900">Exercises</Text>
-      <FitButton.Root variant="primary" onPress={handleRegisterExercise}>
-        <FitButton.Text>Cadastrar Exercício</FitButton.Text>
-      </FitButton.Root>
-    </View>
+    <SafeAreaView className="flex flex-1 flex-col bg-neutral-50 px-5 pt-16">
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={[]}
+        renderItem={({ item, index }) => {
+          return null;
+        }}
+        ItemSeparatorComponent={() => <View className="h-3" />}
+        ListHeaderComponent={() => (
+          <Text className="mb-10 font-openBold text-2xl text-zinc-900">
+            Exercícios Cadastrados:
+          </Text>
+        )}
+        ListFooterComponent={() => (
+          <View className="py-7">
+            <FitButton.Root
+              variant="primary"
+              onPress={() => navigate('RegisterExercise')}
+            >
+              <FitButton.Icon icon={Plus} />
+              <FitButton.Text>Cadastrar Exercício</FitButton.Text>
+            </FitButton.Root>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
- 
