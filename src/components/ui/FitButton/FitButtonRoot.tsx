dacdb827f5477d87351fd0b20db2e274de53c21f
Eduardo Muchak <eduardomuchak@gmail.com>
2023-07-25 09:46:16 -0300

    fix: :art: adjusts fit button border for all variants

--- src/components/ui/FitButton/FitButtonRoot.tsx
+++ src/components/ui/FitButton/FitButtonRoot.tsx
@@ -1,21 +1,21 @@
-import clsx from "clsx";
-import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import clsx from 'clsx';
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 interface FitButtonRootProps extends TouchableOpacityProps {
   children: React.ReactNode;
-  variant: "primary" | "outline" | "ghost";
+  variant: 'primary' | 'outline' | 'ghost';
 }
 
 function FitButtonRoot({ children, variant, ...rest }: FitButtonRootProps) {
   return (
     <TouchableOpacity
       className={clsx(
-        "flex w-full flex-row items-center justify-center rounded-full p-4",
+        'flex w-full flex-row items-center justify-center rounded-full p-3.5',
         {
-          ["bg-yellow-400"]: variant === "primary",
-          ["border-2 border-yellow-400"]: variant === "outline",
-          ["bg-transparent"]: variant === "ghost",
-        }
+          ['border-2 border-yellow-400 bg-yellow-400']: variant === 'primary',
+          ['border-2 border-yellow-400']: variant === 'outline',
+          ['border-2 border-transparent bg-transparent']: variant === 'ghost',
+        },
       )}
       activeOpacity={0.7}
       {...rest}
