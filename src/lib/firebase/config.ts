303da9348fd1334096934b06cce05df15eec8187
Eduardo Muchak <eduardomuchak@gmail.com>
2023-07-31 17:50:50 -0300

    chore: :fire: remove console log

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -24,5 +24,3 @@ const firebaseConfig = {
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
-
-console.log('FIRESTORE_DB', FIRESTORE_DB);
