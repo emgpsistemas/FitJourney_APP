0482df2294b8f5424a126ce0a9a734dcf450383b
Eduardo Muchak <eduardomuchak@gmail.com>
2023-07-31 17:42:22 -0300

    feat: :sparkles: install and setup dotenv and enviroment variables

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,15 +1,24 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from '@env';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCBY3YazxN7Sv__1zXgTt-Qhkj4z_vNoh4',
-  authDomain: 'app-fit-journey.firebaseapp.com',
-  projectId: 'app-fit-journey',
-  storageBucket: 'app-fit-journey.appspot.com',
-  messagingSenderId: '379481782384',
-  appId: '1:379481782384:web:0570f40a59a179aaa11cb5',
-  measurementId: 'G-58G7RPL8FL',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
