8a5f70af6eb2276786be0778b0b4155beec4518c
Eduardo Muchak <eduardomuchak@gmail.com>
2023-08-01 11:30:48 -0300

    feat: :sparkles: adds password validation

--- src/validations/common/UserRegister.ts
+++ src/validations/common/UserRegister.ts
@@ -2,8 +2,16 @@ import { z } from 'zod';
 
 export type UserRegisterFormData = z.infer<typeof userRegisterSchema>;
 
-export const userRegisterSchema = z.object({
-  email: z.string().nonempty('Campo obrigatório').email('E-mail inválido'),
-  password: z.string().nonempty('Campo obrigatório'),
-  confirmPassword: z.string().nonempty('Campo obrigatório'),
-});
+export const userRegisterSchema = z
+  .object({
+    email: z.string().nonempty('Campo obrigatório').email('E-mail inválido'),
+    password: z
+      .string()
+      .nonempty('Campo obrigatório')
+      .min(6, 'A senha deve conter no mínimo 6 caracteres'),
+    confirmPassword: z.string().nonempty('Campo obrigatório'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'As senhas não coincidem',
+    path: ['confirmPassword'],
+  });
