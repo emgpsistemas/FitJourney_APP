55012068af32179ba3764220d294b7d433739c87
Eduardo Muchak <eduardomuchak@gmail.com>
2023-08-24 16:50:02 -0300

    feat: :sparkles: refines edit profile form validation

--- src/validations/User/EditProfile.ts
+++ src/validations/User/EditProfile.ts
@@ -7,8 +7,23 @@ export const editProfileSchema = z.object({
   age: z.string().nonempty('Campo obrigatório'),
   weight: z.string().nonempty('Campo obrigatório'),
   height: z.string().nonempty('Campo obrigatório'),
-  gender: z.string().nonempty('Campo obrigatório'),
-  goal: z.string().nonempty('Campo obrigatório'),
-  fitnessLevel: z.string().nonempty('Campo obrigatório'),
+  gender: z
+    .string()
+    .nonempty('Campo obrigatório')
+    .refine((value) => value !== 'Selecione', {
+      message: 'Campo obrigatório',
+    }),
+  goal: z
+    .string()
+    .nonempty('Campo obrigatório')
+    .refine((value) => value !== 'Selecione', {
+      message: 'Campo obrigatório',
+    }),
+  fitnessLevel: z
+    .string()
+    .nonempty('Campo obrigatório')
+    .refine((value) => value !== 'Selecione', {
+      message: 'Campo obrigatório',
+    }),
   observations: z.string().optional(),
 });
